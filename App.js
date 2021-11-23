bc0a88adec562be85fdad709cc5af07ff8a98de6
Milan sheladiya <30384995+milansheladiya@users.noreply.github.com>
2021-11-23 09:20:51 -0500

    Food Store with API
    food store using yelp API

---
+++ App.js
@@ -0,0 +1,19 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen"
+
+const navigator = createStackNavigator(
+  {
+    SearchScreen: SearchScreen,
+  },
+  {
+    initialRouteName: "SearchScreen",
+    defaultNavigationOptions: {
+      title: "App",
+    },
+  }
+);
+
+
+
+export default createAppContainer(navigator);
